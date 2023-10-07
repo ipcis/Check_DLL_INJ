ced9c91892335ef470c15fd26f801dea7107862a
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-10-07 19:01:45 +0200

    Update hello2.c

--- hello2.c
+++ hello2.c
@@ -1,17 +1,18 @@
 #include <windows.h>
 
-DWORD WINAPI ThreadProc(LPVOID lpParameter) {
-    MessageBox(NULL, "Hello, World!", "Message Box", MB_OK);
-    return 0;
-}
+HINSTANCE hAppInstance;
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        HANDLE hThread = CreateRemoteThread(GetCurrentProcess(), NULL, 0, ThreadProc, NULL, 0, NULL);
-        if (hThread != NULL) {
-            WaitForSingleObject(hThread, INFINITE);
-            CloseHandle(hThread);
-        }
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved) {
+    BOOL bReturnValue = TRUE;
+    switch (dwReason) {
+        case DLL_PROCESS_ATTACH:
+            hAppInstance = hinstDLL;
+            MessageBoxA(NULL, "Hello from DllMain!", "Reflective Dll Injection", MB_OK);
+            break;
+        case DLL_PROCESS_DETACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+            break;
     }
-    return TRUE;
+    return bReturnValue;
 }
