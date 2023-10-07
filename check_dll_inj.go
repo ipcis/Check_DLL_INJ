e9db22ac7dac5501717153163c606904c9cb2137
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-10-07 15:04:41 +0200

    Create check_dll_inj.go

---
+++ check_dll_inj.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "syscall"
+    "unsafe"
+)
+
+const (
+    PROCESS_QUERY_INFORMATION = 0x0400
+    PROCESS_VM_READ           = 0x0010
+)
+
+func main() {
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: dll-inject-checker <process_pid>")
+        os.Exit(1)
+    }
+
+    pid := os.Args[1]
+    targetPID := /* Konvertieren Sie pid in eine numerische PID */
+
+    hProcess, err := syscall.OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, false, uint32(targetPID))
+    if err != nil {
+        fmt.Println("Fehler beim Öffnen des Prozesses:", err)
+        os.Exit(1)
+    }
+    defer syscall.CloseHandle(hProcess)
+
+    var mbi syscall.MEMORY_BASIC_INFORMATION
+    var bytesRead uintptr
+    addr := uintptr(0)
+
+    for {
+        ret, _, _ := syscall.VirtualQueryEx(hProcess, addr, &mbi, unsafe.Sizeof(mbi))
+        if ret == 0 {
+            break
+        }
+
+        if mbi.Type&syscall.MEM_IMAGE != 0 {
+            fmt.Printf("Region Start: %#x, Size: %#x\n", mbi.BaseAddress, mbi.RegionSize)
+        }
+
+        addr = uintptr(unsafe.Pointer(mbi.BaseAddress)) + uintptr(mbi.RegionSize)
+    }
+}
